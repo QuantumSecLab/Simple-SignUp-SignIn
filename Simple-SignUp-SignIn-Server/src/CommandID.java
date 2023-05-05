dab1fa82fd733bd3a56bf1ffdac162cd59afec8b
melody0123 <88380000+melody0123@users.noreply.github.com>
2023-05-05 18:07:50 +0800

    Revert "Revert "Initial version""
    This reverts commit c7be23bfb72101a8854eb1f29343913164635da5.

---
+++ Simple-SignUp-SignIn-Server/src/CommandID.java
@@ -0,0 +1,9 @@
+public class CommandID {
+    public static final int REG_REQ = 1;
+
+    public static final int REG_RESP = 2;
+
+    public static final int LOGIN_REQ = 3;
+
+    public static final int LOGIN_RESP = 4;
+}
